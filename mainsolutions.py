9cd8343e8481379eac0d0be4c9ec84ea0e1de946
TeamBobeezmember1 <s10257017@connect.np.edu.sg>
2023-08-07 14:53:56 +0800

    changes

--- mainsolutions.py
+++ mainsolutions.py
@@ -1,11 +1,3 @@
-"Hello ahdoaihsdoashdoaihs"
-"hi hello ni hao"
-"wasp"
-<<<<<<< HEAD
-"hello Alyssa"
-=======
-"this is grace cheng"
->>>>>>> e018b42e011cf2b59cc87533fa91dc74a6d9597a
 import Overheads, profit_loss
 
 def main():

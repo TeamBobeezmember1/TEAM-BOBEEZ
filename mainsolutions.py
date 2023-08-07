8c3e28ccf036bd1c54b505c9d0c7ccc6fd5f87a9
TeamBobeezmember1 <s10257017@connect.np.edu.sg>
2023-08-07 14:59:55 +0800

    made smthhh

--- mainsolutions.py
+++ mainsolutions.py
@@ -1,6 +1,7 @@
-import Overheads, profit_loss
+import Overheads, profit_loss, coh
 
 def main():
+    coh.coh_function(forex)
     Overheads.overhead_function(forex)
     profit_loss.profitloss_function(forex)
 main()

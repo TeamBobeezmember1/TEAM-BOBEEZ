53f7285bf08eab59fece823b8fb68f7016bbd07b
TeamBobeezmember1 <s10257017@connect.np.edu.sg>
2023-08-07 14:53:18 +0800

    add stuff

--- mainsolutions.py
+++ mainsolutions.py
@@ -6,3 +6,9 @@
 =======
 "this is grace cheng"
 >>>>>>> e018b42e011cf2b59cc87533fa91dc74a6d9597a
+import Overheads, profit_loss
+
+def main():
+    Overheads.overhead_function(forex)
+    profit_loss.profitloss_function(forex)
+main()

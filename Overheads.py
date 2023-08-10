0ab40b62f224072e217672a39938a38635b75fc2
TeamBobeezmember1 <s10257017@connect.np.edu.sg>
2023-08-10 17:55:35 +0800

    added old solution back inside

---
+++ Overheads.py
@@ -0,0 +1,28 @@
+import csv
+
+# Define a dictionary to store the total value for each overhead
+overhead_totals = {}
+
+# Read the CSV file
+with open('Overheads.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        overhead = row['Category']
+        value = float(row['Overheads'])
+
+        # Find total value for each overhead
+        if overhead in overhead_totals:
+            overhead_totals[overhead] += value
+        else:
+            overhead_totals[overhead] = value
+
+# Determine highest overhead
+highest_overhead = max(overhead_totals, key=overhead_totals.get)
+highest_value = overhead_totals[highest_overhead]
+
+# Print the total value for each overhead
+for overhead, total_value in overhead_totals.items():
+    print(f"Total value for {overhead}: {total_value}")
+
+# Print the highest value and corresponding overhead
+print(f"Highest overhead is {highest_overhead}: {highest_value}")
